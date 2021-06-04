c7654d57d69cda7e5d6f4d2e67235708005d6bd0
Jesus Fernando Almanza Loredo <jf_almanza0025@hotmail.com>
2021-06-04 15:03:31 -0500

    Fix with ESLint rules

--- src/app.js
+++ src/app.js
@@ -3,22 +3,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+// Dependencies
 const koa_1 = __importDefault(require("koa"));
+// eslint-disable-next-line import/no-extraneous-dependencies
 const koa_bodyparser_1 = __importDefault(require("koa-bodyparser"));
 const koa2_cors_1 = __importDefault(require("koa2-cors"));
 const koa_logger_1 = __importDefault(require("koa-logger"));
 // Assets
 const hola_1 = __importDefault(require("./routes/hola"));
-const nuevoUsuario_1 = __importDefault(require("./routes/nuevoUsuario"));
+const newUser_1 = __importDefault(require("./routes/newUser"));
 const APP = new koa_1.default();
-const PORT = process.env.PUERTO || 3000;
+const PORT = process.env.PORT || 3000;
 // Middlewares
 APP.use(koa_bodyparser_1.default())
-    .use(koa2_cors_1.default({ origin: "*" }))
+    .use(koa2_cors_1.default({ origin: '*' }))
     .use(koa_logger_1.default())
     .use(hola_1.default.routes())
-    .use(nuevoUsuario_1.default.routes());
+    .use(newUser_1.default.routes());
 // Creacion del servidor
-const SERVER = APP.listen(PORT, async () => console.log(`Server listening on port ${PORT}`)).on("error", (err) => console.log(err));
+const SERVER = APP.listen(PORT, async () => console.log(`Server listening on port ${PORT}`)).on('error', (err) => console.log(err));
 module.exports = SERVER;
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,26 @@
 // Dependencies
-import http from "http";
-import Koa from "koa";
-import bodyParser from "koa-bodyparser";
-import cors from "koa2-cors";
-import logger from "koa-logger";
+import Koa from 'koa';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import bodyParser from 'koa-bodyparser';
+import cors from 'koa2-cors';
+import logger from 'koa-logger';
 
 // Assets
-import hola from "./routes/hola";
-import nuevoUsuario from "./routes/nuevoUsuario";
+import hola from './routes/hola';
+import newUser from './routes/newUser';
 
 const APP: Koa = new Koa();
-const PORT: string | number = process.env.PUERTO || 3000;
+const PORT: string | number = process.env.PORT || 3000;
 
 // Middlewares
 APP.use(bodyParser())
-  .use(cors({ origin: "*" }))
+  .use(cors({ origin: '*' }))
   .use(logger())
   .use(hola.routes())
-  .use(nuevoUsuario.routes());
+  .use(newUser.routes());
 
 // Creacion del servidor
-const SERVER = APP.listen(PORT, async () =>
-  console.log(`Server listening on port ${PORT}`)
-).on("error", (err) => console.log(err));
+const SERVER = APP.listen(PORT, async () => console.log(`Server listening on port ${PORT}`))
+  .on('error', (err) => console.log(err));
 
 module.exports = SERVER;
